1a0b251d59538c03b6a5157bdef793fe096f1d19
max.white <max.beliy2011@mail.ru>
2022-04-20 22:43:03 +0300

    lesson-2

--- src/components/calculator.vue
+++ src/components/calculator.vue
@@ -1,21 +1,12 @@
 <template>
   <div class="calculator">
-    <input
-      type="number"
-      class="calculator__input"
-      @focus="clear($event)"
-      v-model.number="operand1"
-    />
-    <input
-      type="number"
-      class="calculator__input"
-      @focus="clear($event)"
-      v-model.number="operand2"
-    />
-    = {{ result }}
+    <input type="number" class="calculator__input" v-model.number="operand1" />
+    <input type="number" class="calculator__input" v-model.number="operand2" />
+    = {{ result }}<br />
     <div class="error" v-show="error != ''">
       <strong>Внимание!</strong> {{ error }}
     </div>
+    <br />
     <div class="calculator__keyboard">
       <button
         v-for="operator in operators"
@@ -24,6 +15,35 @@
       >
         {{ operator }}
       </button>
+    </div>
+    <br />
+    <label>
+      <input type="checkbox" name="show_keyboard" v-model="screenKeyboard" />
+      Показать экранную клавиатуру
+    </label>
+    <div v-show="screenKeyboard" class="calculator__screen">
+      <br />
+      <div class="calculator__buttons">
+        <button
+          v-for="keyboardKey in keysArray"
+          @click="toOperand('add', keyboardKey)"
+          :key="keyboardKey"
+        >
+          {{ keyboardKey }}
+        </button>
+        <button @click="toOperand('remove')">&larr;</button>
+      </div>
+      <br />
+      <div class="calculator__radio">
+        <label>
+          <input type="radio" value="operand1" v-model="picked" />
+          Операнд 1
+        </label>
+        <label>
+          <input type="radio" value="operand2" v-model="picked" />
+          Операнд 2
+        </label>
+      </div>
     </div>
   </div>
 </template>
@@ -35,15 +55,15 @@ export default {
     return {
       operand1: 0,
       operand2: 0,
+      picked: "operand1",
       result: 0,
       operators: ["+", "-", "/", "*", "^"],
       error: "",
+      screenKeyboard: false,
+      keysArray: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
     };
   },
   methods: {
-    clear(e) {
-      e.target.value = "";
-    },
     calculator(sighn = "+") {
       this.error = "";
       switch (sighn) {
@@ -69,6 +89,23 @@ export default {
         default:
           this.result = 0;
       }
+    },
+    numberToArr(number) {
+      return String(number).split("");
+    },
+    arrToNumber(array) {
+      return Number(array.join(""));
+    },
+    toOperand(action, key) {
+      let numberArray = this.numberToArr(this[this.picked]);
+      switch (action) {
+        case "add":
+          numberArray.push(key);
+          break;
+        default:
+          numberArray.pop();
+      }
+      this[this.picked] = this.arrToNumber(numberArray);
     },
   },
 };
